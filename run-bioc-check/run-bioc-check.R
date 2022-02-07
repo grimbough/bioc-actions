c7b451bfb6017ab6c1ef0870ee1b5a426d44bead
Mike Smith <grimbough@gmail.com>
2022-02-07 09:29:48 +0100

    Add R code as a separate script

---
+++ run-bioc-check/run-bioc-check.R
@@ -0,0 +1,19 @@
+input_arguments <- commandArgs(trailingOnly=TRUE)
+
+if (length(input_arguments) < 2) {
+  stop("At least two argument must be supplied.", call.=FALSE)
+} else if (length(args)==1) {
+  dir <- input_arguments[1]
+  bioccheck_args <- strsplit(tail(input_arguments, -1), "[[:space:],]+")[[1]]
+}
+
+
+
+## strip leading "--" from arguments
+args_list <- as.list(rep(TRUE, length(args)))
+names(args_list) <- gsub(x = args, pattern = "--", replacement = "", fixed = TRUE)
+
+## run BiocCheck
+check_results <- BiocCheck::BiocCheck(package = dir, args_list)
+
+quit(save = "no", status = length(check_results$error)) 
