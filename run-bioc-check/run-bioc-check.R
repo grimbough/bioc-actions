8a5a8b9f2b029ec64a4f2b8fc941de52245bce1a
Mike Smith <grimbough@gmail.com>
2022-02-07 10:34:42 +0100

    Testing Rscript arguments

--- run-bioc-check/run-bioc-check.R
+++ run-bioc-check/run-bioc-check.R
@@ -7,8 +7,6 @@ if (length(input_arguments) < 2) {
   bioccheck_args <- strsplit(tail(input_arguments, -1), "[[:space:],]+")[[1]]
 }
 
-
-
 ## strip leading "--" from arguments
 args_list <- as.list(rep(TRUE, length(args)))
 names(args_list) <- gsub(x = args, pattern = "--", replacement = "", fixed = TRUE)
