3f7c910e18a9062bf2da389c4f9f34bd5873b77a
Mike Smith <grimbough@gmail.com>
2022-02-07 12:07:12 +0100

    Allow setting fail level

--- run-bioc-check/run-bioc-check.R
+++ run-bioc-check/run-bioc-check.R
@@ -1,10 +1,16 @@
 input_arguments <- commandArgs(trailingOnly=TRUE)
 
-if (length(input_arguments) < 2) {
-  stop("At least two argument must be supplied.", call.=FALSE)
+if (length(input_arguments) < 3) {
+  stop("At least three argument must be supplied.", call.=FALSE)
 } else {
   dir <- input_arguments[1]
-  bioccheck_args <- strsplit(tail(input_arguments, -1), "[[:space:],]+")[[1]]
+  error_on <- input_arguments[2]
+  bioccheck_args <- strsplit(tail(input_arguments, -2), "[[:space:],]+")[[1]]
+}
+
+if(!error_on %in% c("error", "warning", "note", "never")) {
+  message("error on argument should be one of: 'error', 'warning', 'note', 'never'")
+  error_on <- "never"
 }
 
 ## strip leading "--" from arguments
@@ -14,4 +20,10 @@ names(args_list) <- gsub(x = bioccheck_args, pattern = "--", replacement = "", f
 ## run BiocCheck
 check_results <- BiocCheck::BiocCheck(package = dir, args_list)
 
-quit(save = "no", status = length(check_results$error)) 
+status <- switch(error_on,
+                 error = length(check_results$error),
+                 warning = length(check_results$warning),
+                 note = length(check_results$note),
+                 never = 0)
+
+quit(save = "no", status = status) 
