095fc7fe3bff580e7645efd04f2e6343beec8df7
Mike Smith <grimbough@gmail.com>
2022-02-07 11:31:06 +0100

    Fix the script

--- run-bioc-check/run-bioc-check.R
+++ run-bioc-check/run-bioc-check.R
@@ -2,14 +2,14 @@ input_arguments <- commandArgs(trailingOnly=TRUE)
 
 if (length(input_arguments) < 2) {
   stop("At least two argument must be supplied.", call.=FALSE)
-} else if (length(args)==1) {
+} else {
   dir <- input_arguments[1]
   bioccheck_args <- strsplit(tail(input_arguments, -1), "[[:space:],]+")[[1]]
 }
 
 ## strip leading "--" from arguments
-args_list <- as.list(rep(TRUE, length(args)))
-names(args_list) <- gsub(x = args, pattern = "--", replacement = "", fixed = TRUE)
+args_list <- as.list(rep(TRUE, length(bioccheck_args)))
+names(args_list) <- gsub(x = bioccheck_args, pattern = "--", replacement = "", fixed = TRUE)
 
 ## run BiocCheck
 check_results <- BiocCheck::BiocCheck(package = dir, args_list)
