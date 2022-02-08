767d4a9938f6b841defbf83d4c348e65c3b544ec
Mike Smith <grimbough@gmail.com>
2022-02-08 09:40:48 +0100

    Allow 0 arguments to BiocCheck

--- run-bioc-check/run-bioc-check.R
+++ run-bioc-check/run-bioc-check.R
@@ -1,14 +1,21 @@
+## We're expecting at least two arguments.
+## 1. The package directory to be checked
+## 2. The condition under which BiocCheck should be deemed to have failed.
+##    One of "error", "warning", "note", "never".
+## 3. Any further arguments are optional and should be passed to BiocCheck
 input_arguments <- commandArgs(trailingOnly=TRUE)
 
-if (length(input_arguments) < 3) {
-  stop("At least three argument must be supplied.", call.=FALSE)
+if (length(input_arguments) < 2) {
+  stop("At least two argument must be supplied.", call.=FALSE)
 } else {
   dir <- input_arguments[1]
   error_on <- input_arguments[2]
-  bioccheck_args <- unlist(strsplit(tail(input_arguments, -2), "[[:space:],]+"))
+  if(length(input_arguments) > 2) {
+    bioccheck_args <- unlist(strsplit(tail(input_arguments, -2), "[[:space:],]+"))
+  } else {
+    bioccheck_args <- NULL
+  }
 }
-
-print(bioccheck_args)
 
 if(!error_on %in% c("error", "warning", "note", "never")) {
   message("error on argument should be one of: 'error', 'warning', 'note', 'never'")
