5a2586af24c88c2cbf8222ee843a7b438b9ac8a3
Charlotte Soneson <charlottesoneson@gmail.com>
2022-04-30 09:30:36 +0200

    Distinguish between 'next' and 'devel' R versions

--- setup-bioc/create-version-map.R
+++ setup-bioc/create-version-map.R
@@ -22,17 +22,26 @@ res <- data.frame(
   rtools
 )
 
-## we need to use the word "devel" rather than specifyin a version number
-## when using r-lib/setup-r to install the devel version of R
+## we need to use the word "devel" or "next" rather than specifying a version 
+## number when using r-lib/setup-r to install the devel or pre-release version 
+## of R
 currentR <- as.numeric_version(rversions::r_release()$version)
 # currentR <- readLines("https://rversions.r-pkg.org/r-release/")
 # currentR <- grep("version", currentR, value = TRUE, fixed = TRUE)
 # currentR <- gsub(".*version.*([0-9]\\.[0-9]+\\.[0-9]+).*", "\\1", x = currentR)
 # currentR <- as.numeric_version(currentR)
 
-devel_R_idx <- which(res$r_version > currentR)
+nextR <- jsonlite::fromJSON("https://api.r-hub.io/rversions/r-next-macos")$version
+nextR <- as.numeric_version(paste(strsplit(nextR, "\\.")[[1]][1:2], collapse = "."))
+
+## For "non-release" R versions, figure out whether they should be "next" 
+## or "devel"
+nonrelease_R_idx <- which(res$r_version > currentR)
+next_R_idx <- intersect(nonrelease_R_idx, which(res$r_version == nextR))
+devel_R_idx <- setdiff(nonrelease_R_idx, next_R_idx)
 
 res$r_version <- as.character(res$r_version)
+res$r_version[next_R_idx] <- "next"
 res$r_version[devel_R_idx] <- "devel"
 
 write.csv(x = res, file = "bioc-version-map.csv", quote = FALSE, row.names = FALSE)
